6e50864ac2a6e04ba7d0afd115da5366b28d1e0f
devansh <devansh@umich.edu>
2023-11-17 17:14:36 -0500

    launch for wifi

---
+++ colcon_ws/src/launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def node(i):
+    return Node(
+            package='ping',
+            namespace=f'pi{i}',
+            executable='ping',
+            name=f'ping_pi{i}', 
+            parameters=[
+                {"ping_rate_hz": 1.0}
+                ]
+            )
+
+def generate_launch_description():
+    return LaunchDescription([ node(i) for i in [2,3,5]])
